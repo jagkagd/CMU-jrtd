c9e600e3b015d564cc4007274c60e2458283e145
jagkagd <jagkagd@gmail.com>
2020-08-26 23:24:23 +0800

    add log module

---
+++ 15-441-project-1/log.c
@@ -0,0 +1,47 @@
+#include "log.h"
+
+int init_log(){
+    if((fp = fopen(logfile, "a")) == NULL){
+        fprintf(stderr, "[ERROR] log file can not be created!");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+void get_ipadress(int sockfd, char *ip_addr, uint *port){
+    struct sockaddr_in cli_addr;
+    socklen_t addr_len;
+    getpeername(sockfd, (struct sockaddr*)&cli_addr, &addr_len);
+    inet_ntop(AF_INET, &cli_addr.sin_addr, ip_addr, INET_ADDRSTRLEN*sizeof(char));
+    *port = ntohs(cli_addr.sin_port);
+}
+
+void get_time(char *time_str) {
+    time_t rawtime;
+    struct tm * timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(time_str, 100, "%d/%b/%Y:%T %z", timeinfo);
+}
+
+void dump_error(int sockfd, const char *msg){
+    char *time_fmt;
+    get_time(time_fmt);
+    char ip_addr[INET_ADDRSTRLEN];
+    uint port;
+    get_ipadress(sockfd, ip_addr, &port);
+    fprintf(fp, "[%s] [error] [client %s:%d] %s", time_fmt, ip_addr, port, msg);
+}
+
+void dump_common(int sockfd, const char *request, int status_code, int size){
+    char *time_fmt;
+    get_time(time_fmt);
+    char ip_addr[INET_ADDRSTRLEN];
+    uint port;
+    get_ipadress(sockfd, ip_addr, &port);
+    fprintf(fp, "%s:%d - [%s] %s %d %d", ip_addr, port, time_fmt, request, status_code, size);
+}
+
+void close_log(){
+    close(fp);
+}
---
+++ 15-441-project-1/log.h
@@ -0,0 +1,29 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+const char* logfile;
+FILE *fp;
+
+int init_log();
+
+void get_time(char *time);
+
+void get_ipadress(int sockfd, struct sockaddr_in *cli_addr, socklen_t *addrlen);
+
+void dump_error(int sockfd, const char *msg);
+
+void dump_common(int sockfd, const char *request, int status_code, int size);
+
+void close_log();
+
+#endif
